bb7ef7543f59c816b23a5dfd36753f94aabd74ce
qcandymanp <hendrikherpers@gmail.com>
2024-02-20 21:55:01 +0100

    Add Navigation and Calendar

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -1,7 +1,73 @@
+import Navigation from '@/app/components/calendar/navigation'
+
+import { Day } from '@/types/calendar/day'
+
+import { useState } from 'react';
+
+const weekdays = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
+
+const getFirstDayOfMonth = (date: Date) => {
+    return new Date(date.getFullYear(), date.getMonth(), 1)
+}
+
+const getLastDayOfMonth = (date: Date) => {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0)
+}
+
+const getNumberOfDaysInMonth = (date: Date) => {
+    return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+}
+
+const getDayGrid = (date: Date) => {
+    const firstDay = getFirstDayOfMonth(date)
+    const lastDay = getLastDayOfMonth(date)
+    const numberOfDays = getNumberOfDaysInMonth(date)
+    const dayGrid = []
+
+    for (let i = 0; i < firstDay.getDay(); i++) {
+        dayGrid.push({
+            day: null,
+            blocked: false
+        } as Day)
+    }
+
+    for (let i = 1; i <= numberOfDays; i++) {
+        dayGrid.push({
+            day: i,
+            blocked: false
+        } as Day)
+    }
+
+    for (let i = lastDay.getDay(); i < 6; i++) {
+        dayGrid.push({
+            day: null,
+            blocked: false
+        } as Day)
+    }
+
+    return dayGrid
+}
+
 export default function Calendar() {
+    const [date, setDate] = useState(new Date())
+
     return (
-        <div>
-            <h1>Calendar</h1>
+        <div className="rounded-lg shadow border-2 w-full bg-white p-4">
+            <Navigation />
+            <div className="flex items-center w-full rounded-t-lg border-0 overflow-hidden">
+                {weekdays.map((day, index) =>
+                    <div key={index} className="flex-1 text-center bg-gray-200 p-4 font-bold">
+                        {day}
+                    </div>
+                )}
+            </div>
+            <div className="grid grid-cols-7 gap-2 p-2 w-full rounded-b-lg bg-gray-200 overflow-hidden">
+                {getDayGrid(date).map((Day, index) =>
+                    <div key={index} className={`flex rounded-lg justify-center flex-col text-just text-center aspect-square p-4 ${Day.blocked ? 'bg-red text-white' : 'bg-gray-300'} ${Day.day == null ? 'opacity-30' : ''}`}>
+                        <span className='text-2xl font-bold'>{Day.day ?? ''}</span>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon as FaIcon } from '@fortawesome/react-fontawesome'
+import * as far from '@fortawesome/free-regular-svg-icons'
+
 import type { Metadata } from "next";
 
 import Calendar from "@/app/components/calendar"
@@ -10,7 +13,10 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <main className="container flex min-h-screen flex-col items-center p-2 mx-auto">
+        <main className="container flex min-h-screen flex-col items-center p-4 mx-auto">
+            <h1 className='text-2xl mb-6'>
+                <FaIcon icon={far.faCalendar} /> Calendar
+            </h1>
             <Calendar />
         </main>
     );
