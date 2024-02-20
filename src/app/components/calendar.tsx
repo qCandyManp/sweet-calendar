e8981ac06b3675bec6084687ff5bb15d7aa36c2c
qcandymanp <hendrikherpers@gmail.com>
2024-02-20 22:33:22 +0100

    Implement basic navigation in the calendar

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -53,7 +53,7 @@ export default function Calendar() {
 
     return (
         <div className="rounded-lg shadow border-2 w-full bg-white p-4">
-            <Navigation />
+            <Navigation date={date} setDate={setDate} />
             <div className="flex items-center w-full rounded-t-lg border-0 overflow-hidden">
                 {weekdays.map((day, index) =>
                     <div key={index} className="flex-1 text-center bg-gray-200 p-4 font-bold">
@@ -63,7 +63,7 @@ export default function Calendar() {
             </div>
             <div className="grid grid-cols-7 gap-2 p-2 w-full rounded-b-lg bg-gray-200 overflow-hidden">
                 {getDayGrid(date).map((Day, index) =>
-                    <div key={index} className={`flex rounded-lg justify-center flex-col text-just text-center aspect-square p-4 ${Day.blocked ? 'bg-red text-white' : 'bg-gray-300'} ${Day.day == null ? 'opacity-30' : ''}`}>
+                    <div key={index} className={`flex rounded-lg justify-center flex-col text-just text-center aspect-video p-4 ${Day.blocked ? 'bg-red text-white' : 'bg-gray-300'} ${Day.day == null ? 'opacity-30' : ''}`}>
                         <span className='text-2xl font-bold'>{Day.day ?? ''}</span>
                     </div>
                 )}
--- src/app/components/calendar/navigation.tsx
+++ src/app/components/calendar/navigation.tsx
@@ -1,7 +1,24 @@
-export default function Navigation() {
+import { Dispatch, SetStateAction } from "react"
+import { FontAwesomeIcon as FaIcon } from "@fortawesome/react-fontawesome"
+import * as far from '@fortawesome/free-regular-svg-icons'
+
+export default function Navigation({ date, setDate }: { date: Date, setDate: Dispatch<SetStateAction<Date>> }) {
+
+    const changeMonth = (value: number) => {
+        setDate(new Date(date.getFullYear(), date.getMonth() + value, 1))
+    }
+
     return (
-        <div>
-            <h1>Navigation</h1>
+        <div className="p-4 flex items-center">
+            <button onClick={() => changeMonth(-1)} className="text-gray-600 text-2xl">
+                <FaIcon icon={far.faArrowAltCircleLeft} />
+            </button>
+            <div className="flex-1 text-center font-bold text-2xl">
+                {date.toLocaleString('default', { month: 'long', year: 'numeric' })}
+            </div>
+            <button onClick={() => changeMonth(1)} className="text-gray-600 text-2xl">
+                <FaIcon icon={far.faArrowAltCircleRight} />
+            </button>
         </div>
     );
 }
