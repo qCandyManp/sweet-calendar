f4c386d156012506d71c56da72495a4120a9647b
qcandymanp <hendrikherpers@gmail.com>
2024-02-22 23:54:34 +0100

    Edit buttons to work with post-css

--- src/app/components/general/button.tsx
+++ src/app/components/general/button.tsx
@@ -1,16 +1,18 @@
+import { MouseEventHandler } from 'react';
+
 export default function Button({
     type,
     className,
-    color = "blue",
-    children
+    children,
+    onClick,
 }: Readonly<{
     type: "button" | "submit" | "reset" | undefined,
     className?: string,
-    color?: string,
-    children: React.ReactNode
+    children: React.ReactNode,
+    onClick?: MouseEventHandler<HTMLButtonElement>;
 }>) {
     return (
-        <button type={type} className={`bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-2 px-4 rounded-lg ${className}`}>
+        <button type={type} onClick={onClick} className={`text-white font-bold py-2 px-4 rounded-lg ${className}`}>
             {children}
         </button>
     )
